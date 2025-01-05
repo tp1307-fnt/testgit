c1b7ecea3c4f9f58cd8b2957560e047a608e40cb
tp1307-fnt <phatthan2098@gmail.com>
2025-01-05 12:01:01 -0800

    con tro hay ho

--- qsort.cpp
+++ qsort.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 // huong dan su dung qsort(stdlib.h) va  comparator 
 /*
  void qsort(void *base, size_t nitems, size_t size, int(*compar)(const void*, const void*))
@@ -18,13 +19,21 @@ int comp(const void *a,const void *b){
 	+> *x - *y cho kq = 1-->khong can sap xep ket qua nhu mang ban dau
 	*/ 
 }
+int tong(int n){
+	int sum=0;
+	while(n){
+		sum = n % 10;
+		n/=10;
+	}
+	return sum;
+}
 /*
 sap xep theo kieu tri tuyet doi tang dan
 6
 -9- 5 4 2 1
 --> 1 2 4 -5 -9
 */
-void qsort1(const void *a,const void *b){
+int comp1(const void *a,const void *b){
 	int *x = (int*)a;
 	int *y = (int*)b;
 	return abs(*x) - abs(*y);
@@ -35,18 +44,22 @@ sap xep tong chu so tang dan
 1 30 29 77 13 9
 1 30 13 9 29 77
 */
-int tong(int n){
-	int sum=0;
-	while(n){
-		sum = n % 10;
-		n/=10;
-	}
-	return sum;
-}
-void qsort2(const void* a, const void* b){
+int comp2(const void* a, const void* b){
 	int *x = (int*)a;
 	int *y = (int*)b;
-	return tong(*x)- tong(*y);
+	return tong(*x)- tong(*y); 
+}
+/*
+6
+111 9 7 21 5 12
+--> 12 21 111 5 7 9
+sap xep theo tong chu so tang dan, neu 2 so co cung tong chu so thi so nho hon se xep truoc
+*/
+int comp3(const void *a, const void* b){
+	int *x = (int*)a;
+	int *y = (int*)b;
+	if(tong(*x) != tong(*y)) return tong(*x) - tong(*y);
+	else return *x - *y;
 }
 void hoanvi(int *a,  int* b){
 	int tmp = *a;
@@ -68,7 +81,7 @@ int main(){
 	for(int i = 0; i < n;i++){
 		scanf("%d", &a[i]);
 	}
-	qsort(a, n, sizeof(int),comp);
+	qsort(a, n, sizeof(int),comp3);
 	for(int i = 0; i < n; i++){
 		printf("%d ", a[i]);
 	}
